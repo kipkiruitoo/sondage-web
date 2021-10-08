65c270502071d59e56e080008fac0c8f71cbd0a9
Davis Too <daviskipkiruitoo@gmail.com>
2021-10-08 21:50:54 +0300

    added Survey model and migrations

---
+++ database/migrations/2021_10_08_184549_create_surveys_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSurveysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('surveys', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->json('survey_json');
+            $table->foreignId('project_id')->constrained();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('surveys');
+    }
+}
