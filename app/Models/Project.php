43f9b5422b13ff85bf3f31ed8142ba8fd81e8c3b
Davis Too <daviskipkiruitoo@gmail.com>
2021-10-08 22:18:11 +0300

    added orchid platform

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,9 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Project extends Model
 {
     use SoftDeletes;
     use HasFactory;
+    use Sluggable;
+
+
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
 }
--- app/Models/Survey.php
+++ app/Models/Survey.php
@@ -5,9 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Survey extends Model
 {
     use SoftDeletes;
     use HasFactory;
+
+
+    use Sluggable;
+
+
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
 }
